8467cc28df0b681c3047d53394e7fe83578a43b5
Vinicius Baessi <viniciusbaessi@gmail.com>
2023-02-01 11:35:28 -0300

    save

--- Meu-site/script.js
+++
@@ -1,8 +0,0 @@
-const botao = document.getElementById('botao');
-
-function toggleMenu () {
-    const nav = document.getElementById('nav');
-    nav.classList.toggleMenu('active');
-}
-
-botao.addEventListener('click', toggleMenu)
